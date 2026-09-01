56e225cc983feee86fe09ae71c003b3534934333
VYOM KUSHVAHA <vyomkushvaha@gmail.com>
2026-09-01 17:43:26 +0530

    Time: 0 ms (100%), Space: 11.4 MB (85.62%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    std::vector<int> plusOne(std::vector<int>& digits) {
+        
+        for (int i = digits.size() - 1; i >= 0; --i) {
+            
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        
+
+        digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};
